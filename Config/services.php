a54f233a1a339a9d7d3b7141392c5a715ed49966
Rahul Shinde <shinde.r.a@gmail.com>
2023-09-14 17:56:46 +0530

    Added service container

---
+++ Config/services.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+
+return static function (ContainerConfigurator $configurator) {
+    $services = $configurator->services()
+        ->defaults()
+        ->autowire()
+        ->autoconfigure();
+
+    $services->load('MauticPlugin\\MailjetBundle\\', '../')
+        ->exclude('../{Config');
+};
