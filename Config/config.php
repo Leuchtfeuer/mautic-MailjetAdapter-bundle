58fed6b8553e14a1c68bbd6fd0a51d9552d8b9f7
Ekkehard Gümbel <43146234+ekkeguembel@users.noreply.github.com>
2026-02-05 17:16:58 +0100

    Update config.php

--- Config/config.php
+++ Config/config.php
@@ -5,6 +5,6 @@ declare(strict_types=1);
 return [
     'name'        => 'Mailjet Adapter by Leuchtfeuer',
     'description' => 'Plugin allows sending emails with Mailjet in batches via API and callback handling used for bounce management.',
-    'version'     => '1.0.11',
+    'version'     => '5.0.0',
     'author'      => 'Leuchtfeuer Digital Marketing GmbH',
 ];
