6c09f6b8ede0caa0d70f72de6297fbc63c73dd9c
Madlen <m.friedrich@leuchtfeuer.com>
2026-05-08 11:47:54 +0200

    Update config.php

--- Config/config.php
+++ Config/config.php
@@ -5,6 +5,6 @@ declare(strict_types=1);
 return [
     'name'        => 'Mailjet Adapter by Leuchtfeuer',
     'description' => 'Plugin allows sending emails with Mailjet in batches via API and callback handling used for bounce management.',
-    'version'     => '5.0.0',
+    'version'     => '5.0.1',
     'author'      => 'Leuchtfeuer Digital Marketing GmbH',
 ];
